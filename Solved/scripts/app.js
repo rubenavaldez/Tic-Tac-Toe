787b0500989828a624180722270b10bcead86ce8
bigchsta62 <bigchsta62@msn.com>
2021-08-03 13:43:08 -0700

    CSS changes

--- Solved/scripts/app.js
+++ Solved/scripts/app.js
@@ -1,38 +1,106 @@
 
-
-
-document.getElementById("top-left").innerText = "Change me"
+// document.getElementById("top-left").innerText = "Change me"
 
 
 
-buttons = document.getElementsByClassName('btn')
+// buttons = document.getElementsByClassName('btn')
 
-buttons =[...buttons] 
-buttons.forEach(button => {
+// buttons =[...buttons] 
+// buttons.forEach(button => {
    
-    button.addEventListener('click', function(event){
+//     button.addEventListener('click', function(event){
 
 
-        console.log(button.value)
+//         console.log(button.value)
+
+//     })    
+// });
 
 
+const statusDisplay = document.querySelector('.game--status');
 
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
 
+statusDisplay.innerHTML = currentPlayerTurn();
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
 
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
 
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
 
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
 
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
 
+    handlePlayerChange();
+}
 
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
 
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
 
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
 
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
 
-
-
-
-    })    
-});
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
--- Unsolved/README.md
+++ Unsolved/README.md
@@ -5,4 +5,28 @@
 
 # User Stories
 
-- The browser tab needs to match the game title
+1. The browser tab needs to match the game title
+
+2. The game needs the traditional 3x3 grid
+
+3. The entire grid will be clickable
+
+4. The grid cells will have the correct player symbol ( X or O ) displayed
+
+5. The UI will display a message indicating which user's turn it is
+
+6. When the game ends it should display who won the game or if the game ended in a draw
+
+7. There will be a restart game button that will restart the entire game
+
+## <em>Hints for your JavaScript:
+   ### <em>Your game should: 
+- <em>track clicks that occur on the cells
+- <em>check if a valid move has been made
+- <em>prevent cells from being selected more than once
+- <em>keep track of and validate game status
+- <em>THEN change the active player
+
+- <em>check if their is a winner or if there is a draw
+- <em>stop the game once there is a win or draw
+- <em>udpate the UI to reflect all of the above
